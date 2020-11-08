7340c1269a0d606a903e9a1084247290b2807757
DestinyBlade <lukas.gonnermann@web.de>
2020-11-08 16:01:51 +0100

    ausgewaehltes UI Fahrzeug Attribut und Java Docs hinzugefuegt

--- app/src/main/java/com/example/tankauswertung/Garage.java
+++ app/src/main/java/com/example/tankauswertung/Garage.java
@@ -2,15 +2,34 @@ package com.example.tankauswertung;
 
 import java.util.Hashtable;
 
+/**
+ * Garage Class verwaltet alle angelegten Fahrzeuge bzw. kann neue Autos hinzufuegen
+ */
 public class Garage {
+    /**
+     * Legt die maximale Anzahl an Fahrzeugen
+     */
     private int maxAnzFahrzeuge;
+    /**
+     * Aktuelle Anzahl an Fahrzeugen in der Garage
+     */
     private int anzFahrzeuge;
+    /**
+     * Hashtable zum speichern der Fahrzeuge in der Garage (geringster Suchaufwand)
+     */
     private Hashtable<Integer, Fahrzeug> fahrzeuge;
+    /**
+     * Momentan ausgewaehltes Fahrzeug im UI
+     */
+    private Fahrzeug ausgewaehltesFahrzeug;
 
+    /**
+     * Default Konstruktor Standard
+     */
     public Garage() {
         this.maxAnzFahrzeuge = 25;
         this.anzFahrzeuge = 0;
-        this.fahrzeuge = new Hashtable<Integer, Fahrzeug>(25);
+        this.fahrzeuge = new Hashtable<>(25);
 
     }
 
@@ -22,7 +41,7 @@ public class Garage {
 
     public void fahrzeugHinzufuegen(Fahrzeug neuAuto) {
         if (anzFahrzeuge <= maxAnzFahrzeuge) {
-            fahrzeuge.put(anzFahrzeuge, neuAuto);
+            fahrzeuge.put(anzFahrzeuge, neuAuto); // TODO
         }
     }
 
@@ -31,7 +50,7 @@ public class Garage {
         if (anzFahrzeuge <= maxAnzFahrzeuge) {
             Fahrzeug neuAuto = new Fahrzeug(pName, pElektro, pVerbrauchAusserorts, pVerbrauchInnerorts, pVerbrauchKombiniert,
                     pKmStand, pTankstand, pCo2Ausstoss);
-            fahrzeuge.put(anzFahrzeuge, neuAuto);
+            fahrzeuge.put(anzFahrzeuge, neuAuto); // TODO
         }
     }
 
