eb58837fce38f622048107c5f5186af72a5f02ae
Yannick Rennett <yannick.rennett@alumni.fh-aachen.de>
2020-11-08 15:12:50 +0100

    Hello Dies ist ein Test und so

--- app/src/main/java/com/example/tankauswertung/Fahrzeug.java
+++ app/src/main/java/com/example/tankauswertung/Fahrzeug.java
@@ -1,4 +1,21 @@
 package com.example.tankauswertung;
 
 public class Fahrzeug {
+    private String name;
+    private boolean elektro;
+    private double verbrauchInnerorts;
+    private double verbrauchAusserorts;
+    private double verbrauchKombiniert;
+    private double kmStand;
+    private int tankstand; //0-100 [%]
+    private double co2Ausstoss;
+
+    public Fahrzeug(String nameIn, boolean elektroIn, double verbrauchAusserortIn, double verbrauchInnerortsIn,
+                    double verbrauchKombiniertIn, double kmStandIn, int tankstandIn, double co2AusstossIn)  {
+        name=nameIn; elektro=elektroIn; verbrauchInnerorts=verbrauchInnerortsIn; verbrauchAusserorts=verbrauchAusserortIn;
+        verbrauchKombiniert=verbrauchKombiniertIn; kmStand=kmStandIn; tankstand=tankstandIn; co2Ausstoss=co2AusstossIn;
+    }
+
+
+
 }
