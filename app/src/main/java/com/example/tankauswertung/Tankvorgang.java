162dcf4e8cbf931892d333598051e461e01bf5f4
Yannick Rennett <yannick.rennett@alumni.fh-aachen.de>
2020-11-08 16:59:45 +0100

    Tankvorgang.class added

---
+++ app/src/main/java/com/example/tankauswertung/Tankvorgang.java
@@ -0,0 +1,37 @@
+package com.example.tankauswertung;
+
+public class Tankvorgang {
+    private double getankteMenge;
+    private double preis;
+    private String img;
+
+    public Tankvorgang(double pGetankteMenge, double pPreis, String pImg) {
+        getankteMenge=pGetankteMenge;
+        preis=pPreis;
+        img=pImg;
+    }
+
+    public void setGetankteMenge(double getankteMenge) {
+        this.getankteMenge = getankteMenge;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public void setPreis(double preis) {
+        this.preis = preis;
+    }
+
+    public double getGetankteMenge() {
+        return getankteMenge;
+    }
+
+    public double getPreis() {
+        return preis;
+    }
+
+    public String getImg() {
+        return img;
+    }
+}
