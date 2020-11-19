551547cf60dc3fdbabb01ac261cbbeaee2ca60b6
Maximilian Bastin <maximilian.bastin@alumni.fh-aachen.de>
2020-11-19 20:08:12 +0100

    JUnit-Test für Klasse Strecke hinzugefügt.

---
+++ app/src/test/java/com/example/tankauswertung/StreckeTest.java
@@ -0,0 +1,39 @@
+package com.example.tankauswertung;
+
+import org.junit.Test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+public class StreckeTest {
+
+    /**
+     * Tests der grundlegenden Funktionen der Strecken
+     */
+    @Test
+    public void testStreckeBasic() {
+        // Aktuelle Uhrzeit zum Vergleich speichern
+        Date aktuelleZeit = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy HH:mm");
+        String aktuelleZeitString = formatter.format(aktuelleZeit);
+
+        // Strecke anlegen
+        Strecke S = new Strecke(24.37, Strecke.Streckentyp.innerorts, 34);
+        // Werte mit Gettern pruefen
+        assertEquals(24.37,S.getDistanz(),0);
+        assertEquals(Strecke.Streckentyp.innerorts,S.getStreckentyp());
+        assertEquals(34,S.getTankstand(),0);
+        assertEquals(aktuelleZeitString,S.getZeitstempelAsString());
+
+        // Strecke bearbeiten
+        S.streckeBearbeiten(20.57, Strecke.Streckentyp.kombiniert,20.5);
+        // Werte mit Gettern pruefen
+        assertEquals(20.57,S.getDistanz(),0);
+        assertEquals(Strecke.Streckentyp.kombiniert,S.getStreckentyp());
+        assertEquals(20.5,S.getTankstand(),0);
+        assertEquals(aktuelleZeitString,S.getZeitstempelAsString());
+
+    }
+}
