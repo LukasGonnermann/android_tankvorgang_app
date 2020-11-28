1199d44d2cebbe4e026963e1c3250944682dc572
Maximilian Bastin <maximilian.bastin@alumni.fh-aachen.de>
2020-11-28 18:19:08 +0100

    JUnit-Tests für Statistiken weiterbearbeitet.

--- app/src/test/java/com/example/tankauswertung/FahrzeugStatistikTest.java
+++ app/src/test/java/com/example/tankauswertung/FahrzeugStatistikTest.java
@@ -1,22 +1,174 @@
 package com.example.tankauswertung;
 
-import android.icu.text.SymbolTable;
+import com.example.tankauswertung.exceptions.FahrzeugWertException;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
+/**
+ * Test der einzelnen Statistik-Methoden
+ * fuer Strecken, Treibstoff, CO2-Ausstoss und Tankkosten
+ * in den unterschiedlichen Intervallen
+ */
 public class FahrzeugStatistikTest {
+    /**
+     * Test der Statistik-Methoden fuer Strecken
+     * Lege ein Fahrzeug an und fuege Strecken hinzu, pruefe ob die Distanz in der Statistik korrekt erfasst wird
+     */
     @Test
-    public void testGetWocheStreckenStatistik() {
+    public void testGetStreckenStatistik() {
+        // Fahrzeug anlegen und Strecken hinzufuegen
         Fahrzeug f = new Fahrzeug("Testfahrzeug", false, 6, 7.5, 7, 27728, 70, 5, 45);
-        f.streckeHinzufuegen(27748, Strecke.Streckentyp.KOMBINIERT, 34);
-        f.streckeHinzufuegen(27768, Strecke.Streckentyp.KOMBINIERT, 34);
-        f.streckeHinzufuegen(27773, Strecke.Streckentyp.KOMBINIERT, 34);
-        double[] ergebnis = f.getWocheStreckenStatistik(0);
-        for (int i = 0; i < ergebnis.length; i++) {
-            System.out.println(i + ": " + ergebnis[i] + "\n");
+        f.streckeHinzufuegen(27748, Strecke.Streckentyp.KOMBINIERT, 75);
+        f.streckeHinzufuegen(27768, Strecke.Streckentyp.AUSSERORTS, 68);
+        f.streckeHinzufuegen(27773, Strecke.Streckentyp.INNERORTS, 52);
+        // Woche
+        // Ergebnis pruefen: Strecke fuer heutigen Tag erfasst, Rest 0
+        double[] ergebnisWoche = f.getWocheStreckenStatistik(0);
+        assertEquals(ergebnisWoche[0], 45, 0);
+        for (int i = 1; i < ergebnisWoche.length; i++) {
+            assertEquals(ergebnisWoche[i], 0, 0);
         }
-        assertEquals(ergebnis[0],45,0);
+
+        // Monat
+        // Ergebnis pruefen: Strecke fuer aktuelle Woche erfasst, Rest 0
+        double[] ergebnisMonat = f.getMonatStreckenStatistik(0);
+        assertEquals(ergebnisMonat[0], 45, 0);
+        for (int i = 1; i < ergebnisMonat.length; i++) {
+            assertEquals(ergebnisMonat[i], 0, 0);
+        }
+
+        // Jahr
+        // Methode noch nicht vorhanden
+
+    }
+
+    /**
+     * Test der Statistik-Methoden fuer verbrauchten Treibstoff
+     * Lege ein Fahrzeug an und fuege Strecken hinzu, pruefe ob der verbrauchte Treibstoff in der Statistik korrekt erfasst wird
+     */
+    @Test
+    public void testGetTreibstoffStatistik() {
+        // Fahrzeug anlegen und Strecken hinzufuegen
+        Fahrzeug f = new Fahrzeug("Testfahrzeug", false, 6, 7.5, 7, 27728, 80, 5, 45);
+        f.streckeHinzufuegen(27748, Strecke.Streckentyp.KOMBINIERT, 75);
+        f.streckeHinzufuegen(27768, Strecke.Streckentyp.AUSSERORTS, 68);
+        f.streckeHinzufuegen(27773, Strecke.Streckentyp.INNERORTS, 52);
+
+        // Verbrauch berechnen
+        double verbrauch = 0;
+        ArrayList<Strecke> strecken = f.getStrecken();
+        for (Strecke item : strecken) {
+            verbrauch += item.getVerbrauchterTreibstoff();
+        }
+
+        // Woche
+        // Verbrauch in Wochenstatistik pruefen
+        double[] ergebnisWoche = f.getWocheTreibstoffStatistik(0);
+        assertEquals(ergebnisWoche[0], verbrauch, 0);
+        for (int i = 1; i < ergebnisWoche.length; i++) {
+            assertEquals(ergebnisWoche[i], 0, 0);
+        }
+
+        // Monat
+        // Verbrauch in Monatsstatistik pruefen
+        double[] ergebnisMonat = f.getMonatTreibstoffStatistik(0);
+        assertEquals(ergebnisMonat[0], verbrauch, 0);
+        for (int i = 1; i < ergebnisMonat.length; i++) {
+            assertEquals(ergebnisMonat[i], 0, 0);
+        }
+
+        // Jahr
+        // Methode noch nicht vorhanden
+
+    }
+
+    /**
+     * Test der Statistik-Methoden fuer CO2-Ausstoss
+     * Lege ein Fahrzeug an und fuege Strecken hinzu, pruefe ob der CO2-Ausstoss in der Statistik korrekt erfasst wird
+     */
+    @Test
+    public void testGetCO2Statistik() {
+        // Fahrzeug anlegen und Strecken hinzufuegen
+        Fahrzeug f = new Fahrzeug("Testfahrzeug", false, 6, 7.5, 7, 27728, 80, 5, 45);
+        f.streckeHinzufuegen(27748, Strecke.Streckentyp.KOMBINIERT, 75);
+        f.streckeHinzufuegen(27768, Strecke.Streckentyp.AUSSERORTS, 68);
+        f.streckeHinzufuegen(27773, Strecke.Streckentyp.INNERORTS, 52);
+
+        // CO2-Ausstoss berechnen
+        double ausstoss = 0;
+        ArrayList<Strecke> strecken = f.getStrecken();
+        for (Strecke item : strecken) {
+            ausstoss += item.getCo2Ausstoss();
+        }
+
+        // Woche
+        // Verbrauch in Wochenstatistik pruefen
+        double[] ergebnisWoche = f.getWocheCO2Statistik(0);
+        assertEquals(ergebnisWoche[0], ausstoss, 0);
+        for (int i = 1; i < ergebnisWoche.length; i++) {
+            assertEquals(ergebnisWoche[i], 0, 0);
+        }
+
+        // Monat
+        // Verbrauch in Monatsstatistik pruefen
+        double[] ergebnisMonat = f.getMonatCO2Statistik(0);
+        assertEquals(ergebnisMonat[0], ausstoss, 0);
+        for (int i = 1; i < ergebnisMonat.length; i++) {
+            assertEquals(ergebnisMonat[i], 0, 0);
+        }
+
+        // Jahr
+        // Methode noch nicht vorhanden
+
+    }
+
+    /**
+     * Test der Statistik-Methoden fuer Tankkosten
+     * Lege ein Fahrzeug an und fuege Tankvorgaenge hinzu, pruefe ob die Tankkosten in der Statistik korrekt erfasst werden
+     */
+    @Test
+    public void testGetTankkostenStatistik() {
+        // Fahrzeug anlegen und Tankvorgaenge hinzufuegen
+        Fahrzeug f = new Fahrzeug("Testfahrzeug", false, 6, 7.5, 7, 27728, 80, 5, 45);
+        try {
+            f.tangvorgangHinzufuegen(4, 8.45, "/DCIM/Camera/meinFoto1.jpg");
+            f.tangvorgangHinzufuegen(27, 32.91, "/DCIM/Camera/meinFoto2.jpg");
+            f.tangvorgangHinzufuegen(44, 78.45, "/DCIM/Camera/meinFoto3.jpg");
+        } catch (FahrzeugWertException e) {
+            e.printStackTrace();
+        }
+
+        // Tankkosten berechnen
+        double kosten = 0;
+        ArrayList<Tankvorgang> tankvorgaenge = f.getTankvorgaenge();
+        for (Tankvorgang item : tankvorgaenge) {
+            kosten += item.getPreis();
+        }
+
+        // Woche
+        // Verbrauch in Wochenstatistik pruefen
+        double[] ergebnisWoche = f.getWocheTankkostenStatistik(0);
+        for (int i = 0; i < ergebnisWoche.length; i++) {
+            System.out.println(i + ": " + ergebnisWoche[i] + "\n");
+        }
+        assertEquals(ergebnisWoche[0], kosten, 0);
+        for (int i = 1; i < ergebnisWoche.length; i++) {
+            assertEquals(ergebnisWoche[i], 0, 0);
+        }
+
+        // Monat
+        // Verbrauch in Monatsstatistik pruefen
+        double[] ergebnisMonat = f.getMonatTankkostenStatistik(0);
+        assertEquals(ergebnisMonat[0], kosten, 0);
+        for (int i = 1; i < ergebnisMonat.length; i++) {
+            assertEquals(ergebnisMonat[i], 0, 0);
+        }
+
+        // Jahr
+        // Methode noch nicht vorhanden
     }
 }
